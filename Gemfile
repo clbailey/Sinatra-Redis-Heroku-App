543c9d27d349675347ebca1b0446d5e77b2264f0
Carter Bailey <carter.l.bailey@gmail.com>
2011-07-23 21:13:27 -0500

    Filled out most of the files, still need .gitignore and Gemfile.lock

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source :gemcutter
+gem 'sinatra', '1.1'
+gem 'redis', '2.0.4'
---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
+get '/' do
+  erb :index
+end
